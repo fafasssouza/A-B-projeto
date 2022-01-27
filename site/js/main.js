b883a3212e4307450758aa07989ecec8e144037e
Fabricio <fafasssouzaoficial@gmail.com>
2022-01-27 18:05:46 -0300

    Adcionado Contato e Rodape

--- site/js/main.js
+++ site/js/main.js
@@ -3,35 +3,49 @@ const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
 
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
-    })
-}
+
+function showMenu() {
+
+    /*===== MENU SHOW =====*/
+    /* Validate if constant exists */
+    if(navToggle){
+        navToggle.addEventListener('click', () =>{
+            navMenu.classList.add('show-menu')
+        })
+    }
+
 
 /* MENU HIDDEN */
-if(navClose){
-    navClose.addEventListener('click', () =>{
+
+
+    if(navClose){
+        navClose.addEventListener('click', () =>{
+            navMenu.classList.remove('show-menu')
+        })
+    }
+}
+
+function removeMenu() {
+
+    /* REMOVE MENU MOBILE */
+    const navLink = document.querySelectorAll('.nav__link')
+
+    function linkAction(){
+        const navMenu = document.getElementById('nav-menu')
+        // When we click on each nav__link, we remove the show-menu class
         navMenu.classList.remove('show-menu')
-    })
+    }
+    navLink.forEach(n => n.addEventListener('click', linkAction))
+
 }
 
-/* REMOVE MENU MOBILE */
-const navLink = document.querySelectorAll('.nav__link')
-
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
+showMenu()
+removeMenu()
 
 //Carousel slider
 function animation_Carousel() {
-    const carouselSlide = document.getElementById("teste")
-    const carouselImage = document.querySelectorAll("#teste img")
+    const carouselSlide = document.getElementById("box_animationID")
+    const carouselImage = document.querySelectorAll("#box_animationID img")
     let counter = 0
 
     function carousel() {
