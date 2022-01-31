85545f68e877d3a442e3ad913d85c44766fd4c52
Fabricio <fafasssouzaoficial@gmail.com>
2022-01-31 16:20:59 -0300

    Atualizando codigo - Corrigindo Erros -

--- site/js/main.js
+++ site/js/main.js
@@ -1,47 +1,3 @@
-/*==================== SHOW MENU ====================*/
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
-
-
-function showMenu() {
-
-    /*===== MENU SHOW =====*/
-    /* Validate if constant exists */
-    if(navToggle){
-        navToggle.addEventListener('click', () =>{
-            navMenu.classList.add('show-menu')
-        })
-    }
-
-
-/* MENU HIDDEN */
-
-
-    if(navClose){
-        navClose.addEventListener('click', () =>{
-            navMenu.classList.remove('show-menu')
-        })
-    }
-}
-
-function removeMenu() {
-
-    /* REMOVE MENU MOBILE */
-    const navLink = document.querySelectorAll('.nav__link')
-
-    function linkAction(){
-        const navMenu = document.getElementById('nav-menu')
-        // When we click on each nav__link, we remove the show-menu class
-        navMenu.classList.remove('show-menu')
-    }
-    navLink.forEach(n => n.addEventListener('click', linkAction))
-
-}
-
-showMenu()
-removeMenu()
-
 //Carousel slider
 function animation_Carousel() {
     const carouselSlide = document.getElementById("box_animationID")
@@ -49,11 +5,11 @@ function animation_Carousel() {
     let counter = 0
 
     function carousel() {
-     counter++
         counter++
+        
         const size = carouselImage[0].clientWidth
 
-        if(counter > carouselImage.length-2) {
+        if(counter > carouselImage.length-4) {
             counter = 0
         }
 
